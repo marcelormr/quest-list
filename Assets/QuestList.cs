9d0ce032a68d23da1943ba9c558980474eb48ba6
Marcelo Rocha <celo.rmr@gmail.com>
2021-04-04 18:21:48 -0300

    Add quest Kennith's Concerns

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -1545,6 +1545,13 @@ public class QuestList : MonoBehaviour
             ),
         isMiniquest: true
         );
+    static Quest kennithsConcerns = new Quest(
+        name: "Kennith's Concerns",
+        difficulty: "Intermediate",
+        questRequirements: new List<Quest> { theSlugMenace },
+        otherRequirements: new OtherRequirements(miningLevel: 46),
+        questPointsReward: 1
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
