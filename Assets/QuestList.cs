ce176f6b77fb67e66910342cb0e74969bc43fbd2
Marcelo Rocha <celo.rmr@gmail.com>
2021-04-07 21:52:15 -0300

    Add quest Rune Mechanics

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -1924,6 +1924,16 @@ public class QuestList : MonoBehaviour
             ),
         questPointsReward: 3
         );
+    static Quest runeMechanics = new Quest(
+        name: "Rune Mechanics",
+        difficulty: "Novice",
+        otherRequirements: new OtherRequirements(
+            magicLevel: 27,
+            runecraftingLevel: 20,
+            constructionLevel: 25
+            ),
+        questPointsReward: 1
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
