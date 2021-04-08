68dd6d3d903bd42e89884b3fd436f04a44c39d70
Marcelo Rocha <celo.rmr@gmail.com>
2021-04-08 11:41:45 -0300

    Add quest Gunnar's Ground

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -2048,6 +2048,13 @@ public class QuestList : MonoBehaviour
             ),
         questPointsReward: 1
         );
+    //This quest replaces Romeo & Juliet
+    static Quest gunnarsGround = new Quest(
+        name: "Gunnar's Ground",
+        difficulty: "Novice",
+        otherRequirements: new OtherRequirements(craftingLevel: 5),
+        questPointsReward: 5 //(only if Romeo & Juliet has not been completed)
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
