0072f06c5896be5be9acbd62ec462d12340c9f84
Marcelo Rocha <celo.rmr@gmail.com>
2021-05-27 16:22:25 -0300

    Update to Unity 2020.3.3f1 Personal

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -2652,6 +2652,8 @@ public class QuestList : MonoBehaviour
             ),
         questPointsReward: 2
         );
+
+    //static Quest 
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
