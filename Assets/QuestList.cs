880e6ba96c0d1a23232fd446ab7266d2a83122f3
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-05 18:30:22 -0300

    Add quest Desperate Measures

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -3284,6 +3284,19 @@ public class QuestList : MonoBehaviour
         difficulty: "Novice",
         questPointsReward: 1
         );
+    static Quest desperateMeasures = new Quest(
+        name: "Desperate Measures",
+        difficulty: "Intermediate",
+        questRequirements: new List<Quest>
+        {
+            desperateTimes
+        },
+        otherRequirements: new OtherRequirements(
+            archeologyLevel: 50,
+            agilityLevel: 50
+            ),
+        questPointsReward: 3
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
