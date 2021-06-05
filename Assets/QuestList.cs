3a32b91b76250193cc5952be64e4ad32fa30fe42
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-05 18:41:26 -0300

    Add miniquest The Vault of Shadows

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -3327,6 +3327,17 @@ public class QuestList : MonoBehaviour
         },
         questPointsReward: 1
         );
+    static Quest theVaultOfShadows = new Quest(
+        name: "The Vault of Shadows",
+        difficulty: "Experienced",
+        otherRequirements: new OtherRequirements(
+            archeologyLevel: 58,
+            divinationLevel: 60
+            //Time Served Mystery
+            //All shadow anchors powered or pylon active
+            ),
+        isMiniquest: true
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
