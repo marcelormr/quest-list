0402eaf4dd46bf7ff79a78256d5e320dd29a4366
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-05 16:56:01 -0300

    Add miniquest Jed Hunter

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -2996,6 +2996,22 @@ public class QuestList : MonoBehaviour
             ),
         isMiniquest: true
         );
+    static Quest jedHunter = new Quest(
+        name: "Jed Hunter",
+        difficulty: "Grandmaster",
+        questRequirements: new List<Quest>
+        {
+            flagFall,
+            spiritualEnlightenment,
+            headOfTheFamily,
+            deadliestCatch
+        },
+        otherRequirements: new OtherRequirements(
+            hunterLevel: 90,
+            craftingLevel: 91
+            ),
+        isMiniquest: true
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
