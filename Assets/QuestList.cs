16cbd14f22d217499850b1caaab755fe4dcd989a
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-05 17:49:54 -0300

    Add quest Violet is Blue

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -3220,6 +3220,11 @@ public class QuestList : MonoBehaviour
         difficulty: "Novice",
         questPointsReward: 1
         );
+    static Quest violetIsBlue = new Quest(
+        name: "Violet is Blue",
+        difficulty: "Novice",
+        questPointsReward: 1
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
