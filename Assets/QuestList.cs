a2118fe9ea3a309843c44a4b2df16bcce4be2938
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-05 17:16:27 -0300

    Add quest Our Man in the North

--- Assets/QuestList.cs
+++ Assets/QuestList.cs
@@ -3151,6 +3151,16 @@ public class QuestList : MonoBehaviour
             ),
         questPointsReward: 1
         );
+    static Quest ourManInTheNorth = new Quest(
+        name: "Our Man in the North",
+        difficulty: "Master",
+        questRequirements: new List<Quest>
+        {
+            doNoEvil,
+            crocodileTears
+        },
+        questPointsReward: 1
+        );
     private void Start()
     {
         List<Skill> skills = new List<Skill> { attack, constitution, mining, strength, agility, smithing, defence, herblore, fishing, ranged, thieving, cooking, prayer, crafting, firemaking, magic, fletching, woodcutting, runecrafting, slayer, farming, construction, hunter, summoning, dungeoneering, divination, invention, archeology };
