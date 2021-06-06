0f4973aee826c50112ff4d4dd6a8e15a82bae267
Marcelo Rocha <celo.rmr@gmail.com>
2021-06-06 19:26:33 -0300

    Move OtherRequirements class to separate file

---
+++ Assets/OtherRequirements.cs
@@ -0,0 +1,59 @@
+public class OtherRequirements
+{
+    byte attackLevel = 1, constitutionLevel = 10, miningLevel = 1,
+         strengthLevel = 1, agilityLevel = 1, smithingLevel = 1,
+         defenceLevel = 1, herbloreLevel = 1, fishingLevel = 1,
+         rangedLevel = 1, thievingLevel = 1, cookingLevel = 1,
+         prayerLevel = 1, craftingLevel = 1, firemakingLevel = 1,
+         magicLevel = 1, fletchingLevel = 1, woodcuttingLevel = 1,
+         runecraftingLevel = 1, slayerLevel = 1, farmingLevel = 1,
+         constructionLevel = 1, hunterLevel = 1, summoningLevel = 1,
+         dungeoneeringLevel = 1, divinationLevel = 1, inventionLevel = 1,
+         archeologyLevel = 1, combatLevel = 3, achievementPoints = 0;
+    ushort questPointsRequired = 0;
+    public OtherRequirements(
+        byte attackLevel = 1, byte constitutionLevel = 10, byte miningLevel = 1,
+        byte strengthLevel = 1, byte agilityLevel = 1, byte smithingLevel = 1,
+        byte defenceLevel = 1, byte herbloreLevel = 1, byte fishingLevel = 1,
+        byte rangedLevel = 1, byte thievingLevel = 1, byte cookingLevel = 1,
+        byte prayerLevel = 1, byte craftingLevel = 1, byte firemakingLevel = 1,
+        byte magicLevel = 1, byte fletchingLevel = 1, byte woodcuttingLevel = 1,
+        byte runecraftingLevel = 1, byte slayerLevel = 1, byte farmingLevel = 1,
+        byte constructionLevel = 1, byte hunterLevel = 1, byte summoningLevel = 1,
+        byte dungeoneeringLevel = 1, byte divinationLevel = 1, byte inventionLevel = 1,
+        byte archeologyLevel = 1, byte combatLevel = 3, byte achievementPoints = 0,
+        ushort questPointsRequired = 0)
+    {
+        this.attackLevel = attackLevel;
+        this.constitutionLevel = constitutionLevel;
+        this.miningLevel = miningLevel;
+        this.strengthLevel = strengthLevel;
+        this.agilityLevel = agilityLevel;
+        this.smithingLevel = smithingLevel;
+        this.defenceLevel = defenceLevel;
+        this.herbloreLevel = herbloreLevel;
+        this.fishingLevel = fishingLevel;
+        this.rangedLevel = rangedLevel;
+        this.thievingLevel = thievingLevel;
+        this.cookingLevel = cookingLevel;
+        this.prayerLevel = prayerLevel;
+        this.craftingLevel = craftingLevel;
+        this.firemakingLevel = firemakingLevel;
+        this.magicLevel = magicLevel;
+        this.fletchingLevel = fletchingLevel;
+        this.woodcuttingLevel = woodcuttingLevel;
+        this.runecraftingLevel = runecraftingLevel;
+        this.slayerLevel = slayerLevel;
+        this.farmingLevel = farmingLevel;
+        this.constructionLevel = constructionLevel;
+        this.hunterLevel = hunterLevel;
+        this.summoningLevel = summoningLevel;
+        this.dungeoneeringLevel = dungeoneeringLevel;
+        this.divinationLevel = divinationLevel;
+        this.inventionLevel = inventionLevel;
+        this.archeologyLevel = archeologyLevel;
+        this.combatLevel = combatLevel;
+        this.achievementPoints = achievementPoints;
+        this.questPointsRequired = questPointsRequired;
+    }
+}
